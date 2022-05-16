83443e41470251e89d7ddf84810ae1a3f0f77b9d
nahidsultan19 <nahidsultan38@gmail.com>
2022-05-16 19:12:03 +0600

    added booking modal.

--- src/Components/AvailableTickets.js
+++ src/Components/AvailableTickets.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
 import Ticket from './Ticket';
+import BookingModal from './BookingModal';
 
 const AvailableTickets = ({ date }) => {
     const [tickets, setTickets] = useState([]);
+    const [bookTicket, setBookTicket] = useState(null);
 
     useEffect(() => {
         fetch('tickets.json')
@@ -14,15 +16,15 @@ const AvailableTickets = ({ date }) => {
     return (
         <div className=''>
             <div className='text-center font-bold text-2xl text-primary'>
-                {date && <p>Available Tickets in {format(date, 'PP')}</p>}
+                {date && <p>Available Tickets on {format(date, 'PP')}</p>}
             </div>
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 my-12 px-10'>
                 {
-                    tickets.map(ticket => <Ticket key={ticket._id} ticket={ticket}></Ticket>)
+                    tickets.map(ticket => <Ticket key={ticket._id} ticket={ticket} setBookTicket={setBookTicket}></Ticket>)
                 }
             </div>
-
+            {bookTicket && <BookingModal bookTicket={bookTicket} setBookTicket={setBookTicket} date={date}></BookingModal>}
         </div>
     );
 };
--- src/Components/Ticket.js
+++ src/Components/Ticket.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
-const Ticket = (props) => {
-    const { name, from, to, time } = props.ticket;
+const Ticket = ({ ticket, setBookTicket }) => {
+    const { name, from, to, time } = ticket;
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl px-10">
             <div class="card-body text-center">
                 <h2 class="text-xl font-bold text-secondary">{name}</h2>
                 <p>{from}-{to}</p>
-                <p>{
-                    time.length > 0 ? <span>{time[0]}</span> : <span>Try another time</span>
-                }</p>
+                <p>
+                    {
+                        time.length > 0 ? <span>{time[0]}</span> : <span>No tickets available</span>
+                    }
+                </p>
+                <p>{time.length} {time.length > 1 ? 'Tickets' : 'Ticket'} Available</p>
                 <div class="card-actions justify-center">
-                    <button class="btn btn-primary">Buy Now</button>
+                    <label onClick={() => setBookTicket(ticket)} htmlFor="booking-modal" class="btn btn-primary">Book Now</label>
                 </div>
             </div>
         </div>
