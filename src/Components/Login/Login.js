3e2d69c513941435b877fe0cc35d49a53e3a04ed
nahidsultan19 <nahidsultan38@gmail.com>
2022-05-21 00:48:49 +0600

    added react hook form inside login form

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,39 +2,76 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { useForm } from "react-hook-form";
 
 
 const Login = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    const onSubmit = data => {
+        console.log(data)
+    };
+
     return (
-        <div class="hero">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <div class="card flex-shrink-0 shadow-2xl bg-base-100">
-                    <div class="card-body">
-                        <div class="form-control">
+
+        <div class="hero-content flex-col lg:flex-row-reverse">
+            <div class="card flex-shrink-0 shadow-2xl bg-base-100">
+                <div class="card-body">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+
+                        <div class="form-control w-full max-w-xs">
                             <label class="label">
                                 <span class="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" class="input input-bordered" />
+                            <input {...register('email', {
+                                required: {
+                                    value: true,
+                                    message: 'Email is required'
+                                },
+                                pattern: {
+                                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                    message: 'Provide a valid email'
+                                }
+                            })} type="email" placeholder="Enter Email" class="input input-bordered w-full max-w-xs" />
+                            <label class="label">
+                                {errors.email?.type === 'required' && <span class="label-text-alt text-red-500">{errors.email.message}</span>}
+                                {errors.email?.type === 'pattern' && <span class="label-text-alt text-red-500">{errors.email.message}</span>}
+
+                            </label>
                         </div>
-                        <div class="form-control">
+                        <div class="form-control w-full max-w-xs">
                             <label class="label">
                                 <span class="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" class="input input-bordered" />
+                            <input {...register('password', {
+                                required: {
+                                    value: true,
+                                    message: 'Password is required'
+                                },
+                                minLength: {
+                                    value: 6,
+                                    message: 'Provide a valid Password'
+                                }
+                            })} type="password" placeholder="Enter Password" class="input input-bordered w-full max-w-xs" />
                             <label class="label">
-                                <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+                                {errors.password?.type === 'required' && <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+                                {errors.password?.type === 'minLength' && <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+
                             </label>
                         </div>
-                        <div class="form-control mt-6">
-                            <button class="btn btn-primary">Login</button>
-                        </div>
-                        <p>Don't have an Account?<Link to='/'>Create an Account</Link></p>
-                        <button onClick={() => signInWithGoogle()} className='btn btn-outline'>Continue with Google</button>
+                        <button className='btn w-full max-w-xs'>Login</button>
+                    </form>
+                    <div class="form-control mt-6">
+                        <button class="btn btn-primary">Login</button>
                     </div>
+                    <p>Don't have an Account?<Link to='/' className='btn btn-link'>Create an Account</Link></p>
+                    <div class="divider">OR</div>
+                    <button onClick={() => signInWithGoogle()} className='btn btn-outline'>Continue with Google</button>
                 </div>
             </div>
-        </div >
+        </div>
+
     );
 };
 
--- src/Shared/Navbar.js
+++ src/Shared/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = () => {
     </>
 
     return (
-        <div class="navbar bg-base-200">
+        <div className="navbar bg-base-200">
             <div class="navbar-start">
                 <div class="dropdown">
                     <label tabindex="0" class="btn btn-ghost lg:hidden">
@@ -40,13 +40,11 @@ const Navbar = () => {
                 <ul class="menu menu-horizontal p-0">
                     {nav}
                 </ul>
-            </div>
-            <div className="navbar-end">
-                <label tabindex="1" for="dashboard-sidebar" class="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                </label>
 
             </div>
+            <label tabindex="1" for="dashboard-sidebar" class="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
         </div>
     );
 };
